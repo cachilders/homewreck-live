7caa2b53774350ee470d8b1d8fb00e91495c21f9
Casey Childers <cachilders@gmail.com>
2020-04-12 12:27:31 -0700

    [feat] stand up placeholder

---
+++ pages/index.js
@@ -0,0 +1,9 @@
+export default () => (
+  <div className="p-4 rounded bg-white">
+    <div>
+      <img class="object-scale-down h-48 w-full" src="/images/sw_logo.jpg" />
+    </div>
+    <h1 className="text-black-700 leading-normal text-center">Homewreck</h1>
+    <p className="text-gray-500 text-center">I'm an uncoordinated husk of a site</p>
+  </div>
+)
