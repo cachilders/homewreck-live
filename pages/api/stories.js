a26cd9a13030811139d5e5a36d2daf3ba2b4fda9
ashermanwmf <shermanandrew50@gmail.com>
2020-04-12 21:03:18 -0700

    added try catch

--- pages/api/stories.js
+++ pages/api/stories.js
@@ -2,11 +2,15 @@ const db = require('../../lib/db')
 const escape = require('sql-template-strings')
 
 module.exports = async (req, res) => {
-  const stories = await db.query(escape`
-    SELECT
-      id,
-      name
-    FROM stories
-  `);
-  res.status(200).json({ stories });
+  try {
+    const stories = await db.query(escape`
+      SELECT
+        id,
+        name
+      FROM stories
+    `);
+    res.status(200).json({ stories });
+  } catch (error) {
+    res.status(500).json({ error, message: 'Something went wrong getting the stories '});
+  }
 };
