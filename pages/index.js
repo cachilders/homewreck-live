ab48801fd3ee96fae0c6249cb5cb79507bc6ed63
Zack Fernandes <zack@ohnoco.com>
2020-04-12 20:48:59 -0700

    Spruce up index
    - Include a <title> element using next/head
    - Fix instances of `class` and rename to `className`
    - Change `for` → `htmlFor` on the label

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,14 @@
+import Head from 'next/head'
+
 export default () => (
   <div className="w-full max-w-xs m-auto mt-10 p-4">
+    <Head>
+      <title>Homewreck: Emma - Login to vote</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+
     <div>
-      <img class="object-scale-down h-48 w-full" src="/images/sw_logo.jpg" />
+      <img className="object-scale-down h-48 w-full" src="/images/sw_logo.jpg" />
     </div>
     <h1 className="text-black-700 leading-normal text-center">
       Homewreck: <b>Emma</b>
@@ -9,19 +16,19 @@ export default () => (
 
     <div className="mt-10">
       <form className="bg-gray-100 border shadow-md rounded px-8 pt-6 pb-8 mb-4" action="/api/login" method="post">
-        <div class="mb-4">
-          <label class="block text-gray-700 text-sm font-bold mb-2" for="email">
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="email">
             Email
           </label>
-          <input class="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+          <input className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
             id="email"
             name="email"
             type="email"
             placeholder="Email"
           />
         </div>
-        <div class="flex items-center justify-between">
-          <input class="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 w-full rounded focus:outline-none focus:shadow-outline"
+        <div className="flex items-center justify-between">
+          <input className="bg-red-500 hover:bg-red-600 text-white font-bold py-2 px-4 w-full rounded focus:outline-none focus:shadow-outline"
             type="submit"
             value="Start Voting"
           />
