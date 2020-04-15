80170a80ec769474524576823f28c69913fb8cb6
Andrew Sherman <shermanandrew50@gmail.com>
2020-04-14 23:55:51 -0700

    thank you page (#9)
    * thank you page
    
    * dot
    
    Co-authored-by: Casey A Childers <cachilders@users.noreply.github.com>

---
+++ pages/components/ImagePage.js
@@ -0,0 +1,9 @@
+export default ({ subtext }) => (
+  <div className="p-4 rounded bg-white">
+    <div>
+      <img class="object-scale-down h-48 w-full" src="/images/sw_logo.jpg" />
+    </div>
+    <h1 className="text-black-700 leading-normal text-center">Homewreck</h1>
+    <p className="text-gray-500 text-center">{subtext}</p>
+  </div>
+)
---
+++ pages/thank_you.js
@@ -0,0 +1,5 @@
+import ImagePage from './components/ImagePage';
+
+export default () => (
+  <ImagePage subtext={'Let\'s not wreck this home any more. Thanks for the votes!'} />
+)
