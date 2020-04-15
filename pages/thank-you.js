3194ef42ea3e1173d98f09b61178626bd6555e2f
Casey A Childers <cachilders@users.noreply.github.com>
2020-04-15 00:34:13 -0700

    underscore meets hyphen (#12)
    * underscore meets hyphen
    
    * style consistency
    
    * import

---
+++ pages/thank-you.js
@@ -0,0 +1,12 @@
+import Head from 'next/head'
+import ImageCard from './components/ImageCard';
+
+export default () => (
+  <div className="w-full max-w-xs m-auto mt-10">
+    <Head>
+      <title>Homewreck: Emma - Your vote has been counted</title>
+      <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+    </Head>
+    <ImageCard subtext={'Let\'s not wreck this home any more. Thanks for the votes!'} />
+  </div>
+)
--- pages/thank_you.js
+++
@@ -1,5 +0,0 @@
-import ImagePage from './components/ImagePage';
-
-export default () => (
-  <ImagePage subtext={'Let\'s not wreck this home any more. Thanks for the votes!'} />
-)
