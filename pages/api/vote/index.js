744738e9fba48b648ff03ac88a17cfb7431502f1
Casey Childers <cachilders@gmail.com>
2020-04-14 23:40:21 -0700

    add vote icrement, multiplier, and basic redirect

--- pages/api/vote/index.js
+++ pages/api/vote/index.js
@@ -3,18 +3,24 @@ const escape = require('sql-template-strings')
 
 module.exports = async (req, res) => {
   const {
-    votes: { first, second, third },
-    voterId
+    votes: { first, second, third, multiplier },
+    voterId,
   } = JSON.parse(req.body)
+
   try {
-    const result = await db.query(escape`
+    const voteResult = await db.query(escape`
       INSERT INTO votes (voter_id, stories_id, score)
       VALUES
-        (${voterId},${first}, 3),
-        (${voterId},${second}, 2),
-        (${voterId},${third}, 1)
+        (${voterId},${first}, ${3 * multiplier}),
+        (${voterId},${second}, ${2 * multiplier}),
+        (${voterId},${third}, ${1 * multiplier})
     `);
-    res.status(200).json({ result });
+    const voterUpdateResult = await db.query(escape`
+      UPDATE voters
+        SET votes_made = votes_made + ${multiplier}
+        WHERE id = ${voterId}
+    `);
+    res.status(200).json({ voteResult, voterUpdateResult });
   } catch (error) {
     res.status(500).json({ error, message: 'Something went wrong submitting your vote'});
   }
--- pages/ballot.js
+++ pages/ballot.js
@@ -1,26 +1,41 @@
 import fetch from 'isomorphic-unfetch'
-import { useState } from 'react'
+import Router from 'next/router'
+import { useState, useEffect } from 'react'
 
 Ballot.getInitialProps = async ({ req, query }) => {
-  const protocol = req
-    ? `${req.headers['x-forwarded-proto'] || 'http'}:`
-    : location.protocol
-  const host = req ? req.headers['x-forwarded-host'] || req.headers['host'] : location.host
-  const res = await fetch(`${protocol}//${host}/api/ballot`)
-  const json = await res.json()
-  return {
-    protocol,
-    host,
-    voterId: query.sup,
-    ...json,
+  const id = query && query.shipster
+  if (id) {
+    const protocol = req
+      ? `${req.headers['x-forwarded-proto'] || 'http'}:`
+      : location.protocol
+    const host = req ? req.headers['x-forwarded-host'] || req.headers['host'] : location.host
+    const res = await fetch(`${protocol}//${host}/api/ballot?id=${id}`)
+    const json = await res.json()
+  
+    return {
+      protocol,
+      host,
+      ...json,
+    }
   }
+  return {}
 }
 
-const chevron = <svg className="fill-current h-4 w-4" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/></svg>
+const defaultBallot = { first: '', second: '', third: '', multiplier: 1 }
+const chevron = (
+  <svg
+    className="fill-current h-4 w-4"
+    xmlns="http://www.w3.org/2000/svg"
+    viewBox="0 0 20 20">
+    <path d="M9.293 12.95l.707.707L15.657 8l-1.414-1.414L10 10.828 5.757 6.586 4.343 8z"/>
+  </svg>
+)
 
-function Ballot({stories, voterId, protocol, host}) {
-  const [votes, setVotes] = useState({ first: '', second: '', third: '' })
-  const { first, second, third } = votes;
+function Ballot({stories = [], voter = {}, protocol, host}) {
+  const { id: voterId, total_votes: totalVotes, votes_made: votesMade } = voter
+  const [votes, setVotes] = useState(defaultBallot)
+  const [maxVotes, setMaxVotes] = useState(totalVotes - votesMade)
+  const { first, second, third, multiplier } = votes;
   const selected = Object.values(votes);
   const voteDisabled = selected.includes('');
   const handleSelect = ({target: { id, value } }) => setVotes({ ...votes, [id]: value})
@@ -35,23 +50,45 @@ function Ballot({stories, voterId, protocol, host}) {
       method: 'POST',
       mode: 'no-cors',
       body: JSON.stringify({
-        votes: { first, second, third },
-        voterId: voterId
+        voterId,
+        votes: { first, second, third, multiplier },
       })
     })
-    const json = await res.json()
-    console.log(json)
+
+    if (res.status === 200) {
+      const nextVotes = maxVotes - multiplier
+      if (nextVotes < 1) Router.push('/thank-you')
+      setVotes(defaultBallot)
+      setMaxVotes(nextVotes)
+    }
   }
   
+  useEffect(() => {
+    if (!voter.id) Router.push('/')
+  });
+
   return (
     <div className="w-full max-w-xs m-auto mt-10">
+      <div class="max-w-sm rounded overflow-hidden shadow-lg">
+        <img class="w-full" src="/images/sw_logo.jpg" alt="It'll be weird" />
+        <div class="px-6 py-4">
+          <div class="font-bold text-xl mb-2">Your Ballot</div>
+          <p class="text-gray-700 text-base">
+            OK. Listen. You bought {totalVotes} votes, and you've got {maxVotes} remaining.
+          </p>
+        </div>
+      </div>
       <form className="bg-gray-100 border shadow-md rounded px-8 pt-6 pb-8 mb-4">
         <div className="mb-4">
           <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="first">
             Most Favorite
           </label>
           <div className="inline-block relative w-64">
-            <select value={first} onChange={handleSelect} id="first" className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+            <select
+              value={first} onChange={handleSelect}
+              id="first"
+              className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            >
               <option disabled value='' />
               {options}
             </select>
@@ -65,7 +102,12 @@ function Ballot({stories, voterId, protocol, host}) {
             Second Most Favorite
           </label>
           <div className="inline-block relative w-64">
-            <select value={second} onChange={handleSelect} id="second" className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+            <select
+              value={second}
+              onChange={handleSelect}
+              id="second"
+              className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            >
               <option disabled value='' />
               {options}
             </select>
@@ -79,9 +121,32 @@ function Ballot({stories, voterId, protocol, host}) {
             Third Most Favorite
           </label>
           <div className="inline-block relative w-64">
-            <select value={third} onChange={handleSelect} id="third" className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline">
+            <select
+              value={third}
+              onChange={handleSelect}
+              id="third"
+              className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            >
               <option disabled value='' />
               {options}
+            </select>
+            <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
+              {chevron}
+            </div>
+          </div>
+        </div>
+        <div className="mb-4">
+          <label className="block text-gray-700 text-sm font-bold mb-2" htmlFor="third">
+            Number of votes to use on this ballot
+          </label>
+          <div className="inline-block relative w-64">
+            <select
+              value={multiplier}
+              onChange={handleSelect}
+              id="multiplier"
+              className="block appearance-none w-full bg-white border border-gray-400 hover:border-gray-500 px-4 py-2 pr-8 rounded shadow leading-tight focus:outline-none focus:shadow-outline"
+            >
+              {maxVotes && Array.from(Array(maxVotes).keys()).map((v) => <option value={v+1}>{v+1}</option>)}
             </select>
             <div className="pointer-events-none absolute inset-y-0 right-0 flex items-center px-2 text-gray-700">
               {chevron}
@@ -91,11 +156,11 @@ function Ballot({stories, voterId, protocol, host}) {
         <div className="flex items-center justify-between">
           <button
             className={`${voteDisabled ? 'bg-gray-500' : 'bg-red-500 hover:bg-red-600'} text-white font-bold py-2 px-4 w-full rounded focus:outline-none focus:shadow-outline`}
-            type="submit"
+            type="button"
             onClick={castVote}
             disabled={voteDisabled}
           >
-            Cast
+            Cast it!
           </button>
         </div>
       </form>
