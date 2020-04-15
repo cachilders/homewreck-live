f307d7f7dd4a0eee7f446c5cf48aae15c944aa5b
Casey A Childers <cachilders@users.noreply.github.com>
2020-04-14 23:53:33 -0700

    dot

--- pages/thank_you.js
+++ pages/thank_you.js
@@ -1,4 +1,4 @@
-import ImagePage from '../components/ImagePage';
+import ImagePage from './components/ImagePage';
 
 export default () => (
   <ImagePage subtext={'Let\'s not wreck this home any more. Thanks for the votes!'} />
